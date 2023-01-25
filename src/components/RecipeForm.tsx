9bb154851b59a78459f002a3b7f7c7ba038205f0
Marken Foo <41288521+Marken-Foo@users.noreply.github.com>
2023-01-25 17:45:07 +0800

    Use CSS grid to layout RecipeForm

--- src/components/RecipeForm.tsx
+++ src/components/RecipeForm.tsx
@@ -1,6 +1,6 @@
 import '@components/RecipeForm.css';
 import { Dlc, DlcChoice, SelectMode } from '@/App'; // types
-import { DlcFormSection } from '@components/DlcFormSection';
+import { CheckboxWithCaption } from '@components/CheckboxWithCaption';
 import { RadioButtonWithCaption } from '@components/RadioButtonWithCaption';
 import { TagText, TagType } from '@components/Tag'; // types
 import { TagPicker } from '@components/TagPicker';
@@ -65,42 +65,47 @@ export const RecipeForm = ({
 }: RecipeFormProps) => {
   return (
     <div className="recipeForm">
-      <DlcFormSection
-        dlcs={dlcs}
-        dlcVersions={dlcVersions}
-        setDlcVersions={setDlcVersions}
+      <span className="formLabel">持有游戏内容：</span>
+      <span className="formInput">
+        {dlcs.map((dlc) => (
+          <CheckboxWithCaption
+            initialState={dlcVersions[dlc.name as keyof DlcChoice]}
+            onChange={() =>
+              setDlcVersions((prevState) => ({
+                ...prevState,
+                [dlc.name]: !prevState[dlc.name as keyof DlcChoice],
+              }))
+            }
+            label={dlc.label}
+            key={dlc.name}
+          />
+        ))}
+      </span>
+      <span className="formLabel">正特性筛选：</span>
+      <TagPicker
+        tags={tags}
+        selectedTags={selectedTags}
+        setSelectedTags={setSelectedTags}
+        tagType={TagType.FOOD}
       />
-      <div className="tagSection">
-        <span className="tagsLabel">正特性筛选：</span>
-        <TagPicker
-          tags={tags}
-          selectedTags={selectedTags}
-          setSelectedTags={setSelectedTags}
-          tagType={TagType.FOOD}
-        />
-        <MatchModeSelector
-          selectMode={selectMode}
-          setSelectMode={setSelectMode}
-        />
-      </div>
-      <div className="tagSection">
-        <span className="tagsLabel">负特性筛选：</span>
-        <TagPicker
-          tags={tags}
-          selectedTags={selectedIncompatibleTags}
-          setSelectedTags={setSelectedIncompatibleTags}
-          tagType={TagType.BAD}
-        />
-      </div>
-      <div className="tagSection">
-        <span className="tagsLabel">排除正特性：</span>
-        <TagPicker
-          tags={tags}
-          selectedTags={unwantedTags}
-          setSelectedTags={setUnwantedTags}
-          tagType={TagType.STRIKETHROUGH}
-        />
-      </div>
+      <MatchModeSelector
+        selectMode={selectMode}
+        setSelectMode={setSelectMode}
+      />
+      <span className="formLabel">负特性筛选：</span>
+      <TagPicker
+        tags={tags}
+        selectedTags={selectedIncompatibleTags}
+        setSelectedTags={setSelectedIncompatibleTags}
+        tagType={TagType.BAD}
+      />
+      <span className="formLabel">排除正特性：</span>
+      <TagPicker
+        tags={tags}
+        selectedTags={unwantedTags}
+        setSelectedTags={setUnwantedTags}
+        tagType={TagType.STRIKETHROUGH}
+      />
     </div>
   );
 };
