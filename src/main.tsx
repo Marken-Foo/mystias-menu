34f96d9f79132cbcd1dfa9da4bd2eab2be3c3f8c
Marken Foo <41288521+Marken-Foo@users.noreply.github.com>
2023-03-22 22:54:22 +0800

    Install react-router

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import * as React from 'react';
 
-import ReactDOM from 'react-dom/client';
+import * as ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import App from '@/App';
 // import './index.css';
 import '@/i18n';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
